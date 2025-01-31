e2d690aafa9713c034b749377a47d67263d29f32
lukeknofczynski <lknofczynski@unomaha.edu>
2025-01-31 01:41:11 +0000

    Finished Magic8Ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -13,16 +13,19 @@ def main():
 answers = ["Yes", "Not Likely", "Ask Again Later",
            "Don't Count on it", "My Sources Say No", "Very Likely",
            "Without a Doubt", "No", "It is Certain", "Very Doubtful",
-            "Signs Point to Yes"]
+           "Signs Point to Yes", "You May Rely on It", "Outlook not so Good",
+           "Outlook Good", "Cannot Predict Right Now", "As I see it, Yes",
+           "Better not Tell You Now", "Concentrate and Ask Again", "It is Decidedly So",
+           "My Reply is No"]
 
 
   #Answer question randomly with one of the options from your earlier list.
 
 num = random.random()
-num = num * 1000
+num = num * 100
 num = int(num)
-
-
+num = num//5
+num = int(num)
 question = input("Ask me a yes or no question: ")
 print(answers[num])
 if __name__ == '__main__':
